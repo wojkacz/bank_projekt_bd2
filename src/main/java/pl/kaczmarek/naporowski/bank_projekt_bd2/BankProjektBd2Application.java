71b1bff98e5c972392116de9075712d48b089041
wojkacz <252813@student.pwr.edu.pl>
2021-12-10 19:55:28 +0100

    Initial commit

---
+++ src/main/java/pl/kaczmarek/naporowski/bank_projekt_bd2/BankProjektBd2Application.java
@@ -0,0 +1,13 @@
+package pl.kaczmarek.naporowski.bank_projekt_bd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankProjektBd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankProjektBd2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kaczmarek/naporowski/bank_projekt_bd2/BankProjektBd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kaczmarek.naporowski.bank_projekt_bd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankProjektBd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
